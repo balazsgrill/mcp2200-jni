05d2bc8989e406838c4cadd029cb22624ed8f38d
balazsgrill <balage.g@gmail.com>
2014-02-05 13:32:53 +0000

    build script generates jni header on the fly

--- Build.sh
+++ Build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ${JAVA_HOME:=/usr/lib/jvm/default-java}
+ant -f Build.xml jni_header
 export CC_OPT="-fPIC '-I$JAVA_HOME/include' '-I$JAVA_HOME/include/linux'"
 export SO_EXT=".so"
 export LD_LIBS="-lrt -lpthread -ludev -lusb-1.0"
 export LD_OPT="-shared"
 make 
 mv bin-c/mcp2200-jni.so bin-c/libmcp2200-jni.so
+ant -f Build.xml
