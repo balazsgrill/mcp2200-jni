81dbc696d497c74c72a211a10923198ef7123dee
Balázs Grill <balage.g@gmail.com>
2014-02-05 13:54:17 +0100

    compilation scripts updated

--- Build.sh
+++ Build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ${JAVA_HOME:=/usr/lib/jvm/default-java}
-export CC_OPT="'-I$JAVA_HOME/include' '-I$JAVA_HOME/include/linux'"
+export CC_OPT="-fPIC '-I$JAVA_HOME/include' '-I$JAVA_HOME/include/linux'"
 export SO_EXT=".so"
 export LD_LIBS="-lrt -lpthread -ludev -lusb-1.0"
 export LD_OPT="-shared"
