d2f25d479d894173274349cec19ae8a5d9bb6ea7
Balázs Grill <balage.g@gmail.com>
2014-02-11 07:39:22 +0100

    Message text added to exception

--- src/hu/mcp2200/MCP2200Exception.java
+++ src/hu/mcp2200/MCP2200Exception.java
@@ -11,9 +11,24 @@ public class MCP2200Exception extends Exception {
 
 	public static String getMessage(int code){
 		switch (code) {
-		case -101:
-			return "";
-
+		case 0: return "LIBUSB_SUCCESS";
+		case -1: return "LIBUSB_ERROR_IO";
+		case -2: return "LIBUSB_ERROR_INVALID_PARAM";
+		case -3: return "LIBUSB_ERROR_ACCESS";
+		case -4: return "LIBUSB_ERROR_NO_DEVICE";
+		case -5: return "LIBUSB_ERROR_NOT_FOUND";
+		case -6: return "LIBUSB_ERROR_BUSY";
+		case -7: return "LIBUSB_ERROR_BUSY";
+		case -8: return "LIBUSB_ERROR_OVERFLOW";
+		case -9: return "LIBUSB_ERROR_PIPE";
+		case -10: return "LIBUSB_ERROR_INTERRUPTED";
+		case -11: return "LIBUSB_ERROR_NO_MEM";
+		case -12: return "LIBUSB_ERROR_NOT_SUPPORTED";
+		case -99: return "LIBUSB_ERROR_OTHER";
+		
+		case -101: return "MCP2200_INVALID_CONNECTION_ID";
+		case -102: return "MCP2200_INVALID_RESPONSE";
+		case -103: return "MCP2200_IO_ERROR";
 		default:
 			return "Unknown error ("+code+")!";
 		}
